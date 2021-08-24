9834712ff2ea92cbf988fbe0b82ffc72d1fe7ef6
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2021-08-25 00:00:49 +0200

    My Webview App

---
+++ app/src/main/java/com/development/mywebviewapp/MainActivity.java
@@ -0,0 +1,100 @@
+package com.development.mywebviewapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.KeyEvent;
+import android.webkit.WebBackForwardList;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    WebViewClient mywebViewClient;
+
+    WebBackForwardList myWebBackForwardList;
+
+    int progress = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myWebView = new WebView(this);
+
+        setContentView(myWebView);
+
+        if(savedInstanceState != null)
+        {
+            myWebView.loadUrl((String)savedInstanceState.getString("URL"));
+
+        }
+
+        else
+        {
+            myWebView.loadUrl("https://fantasy.premierleague.com");
+        }
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        mywebViewClient = new WebViewClient();
+
+        myWebView.setWebViewClient(mywebViewClient);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack())
+        {
+            myWebView.goBack();
+
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+        String url = myWebView.getUrl();
+
+        progress = myWebView.getProgress();
+
+        outState.putString("URL",url);
+    }
+
+    /*  public class MyWebViewCLient extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url)
+        {
+            if("www.google.com".equals(Uri.parse(url)
+               .getHost()))
+            {
+                return false;
+            }
+            else
+            {
+               Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+
+               startActivity(intent);
+
+                return true;
+            }
+        }
+    }*/
+}
